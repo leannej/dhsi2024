116b8f598444aa2e0d4dae644f862b31c87b7c06
leannej <68397483+leannej@users.noreply.github.com>
2024-06-07 09:53:56 -0700

    fixed

--- sketch/mySketch.js
+++ sketch/mySketch.js
@@ -41,7 +41,7 @@ var grammarSource = {
     
   ],
   "origin": [
-    "Ask the bird the #breed#, to #result#, then #feeling#"
+    "Ask the bird the #breed# to #result# then #feeling#"
   ],
   "breed": [
     "Dodo",
